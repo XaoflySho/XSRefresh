6b30d432aefd75918f17e52cb96bc2ba2926fe4b
Xaofly Sho <shaoxiaof@hotmail.com>
2020-03-20 17:12:52 +0800

    Example initial

---
+++ XSRefreshExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XSRefreshExample
+//
+//  Created by 邵晓飞 on 2020/3/20.
+//  Copyright © 2020 zlucy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
