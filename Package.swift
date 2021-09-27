322fe7b076732c7c6521be301921c6948af5e2ae
Xaofly Sho <shaoxiaof@hotmail.com>
2021-09-27 16:19:50 +0800

    Set localization

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "XSRefresh",
+    defaultLocalization: "en",
     platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
